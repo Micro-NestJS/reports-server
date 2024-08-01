238750a6cec3d5601781a65828247f45cfbe6199
angelstchavez <angelstchavez@gmail.com>
2024-08-01 13:01:35 -0500

    feat: implement pdfmake reporter

--- src/basic-reports/basic-reports.service.ts
+++ src/basic-reports/basic-reports.service.ts
@@ -1,5 +1,16 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import PdfPrinter from 'pdfmake';
+import { TDocumentDefinitions } from 'pdfmake/interfaces';
+
+const fonts = {
+  Roboto: {
+    normal: 'fonts/Roboto-Regular.ttf',
+    bold: 'fonts/Roboto-Bold.ttf',
+    italics: 'fonts/Roboto-Italic.ttf',
+    bolditalics: 'fonts/Roboto-MediumItalic.ttf',
+  },
+};
 
 @Injectable()
 export class BasicReportsService extends PrismaClient implements OnModuleInit {
@@ -8,5 +19,15 @@ export class BasicReportsService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
     this.logger.log('Database connected');
+  }
+
+  hello() {
+    const printer = new PdfPrinter(fonts);
+    const docDefinition: TDocumentDefinitions = {
+      content: ['Hello World'],
+    };
+    const doc = printer.createPdfKitDocument(docDefinition);
+
+    return doc;
   }
 }
